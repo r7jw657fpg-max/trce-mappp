9ad74ac5f72eeb582bce098fd14fe89d4873cb3c
r7jw657fpg-max <r7jw657fpg@privaterelay.appleid.com>
2026-08-28 10:32:28 +0200

    Update _middleware.js

--- functions/admin/_middleware.js
+++ functions/admin/_middleware.js
@@ -1,1 +1,24 @@
+export async function onRequest(context) {
+  const { request, env } = context;
+  const auth = request.headers.get("Authorization");
 
+  if (auth) {
+    const [scheme, encoded] = auth.split(" ");
+
+    if (scheme === "Basic" && encoded) {
+      const decoded = atob(encoded);
+      const [, password] = decoded.split(":");
+
+      if (password === env.ADMIN_PASSWORD) {
+        return context.next();
+      }
+    }
+  }
+
+  return new Response("Zugriff verweigert", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Admin Bereich"',
+    },
+  });
+}
